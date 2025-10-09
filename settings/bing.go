b81f710805f9083ecdb76e660bd5640e9b0e88cf
Beeson <131391958+Beesonn@users.noreply.github.com>
2025-10-09 16:20:18 +0530

    Update bing.go

--- settings/bing.go
+++ settings/bing.go
@@ -1,7 +1,7 @@
 package settings
 
 import (
-    "encoding/json"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"strings"
@@ -10,39 +10,39 @@ import (
 )
 
 type ImageObject struct {
-	URL string `json:"url"` 
+	URL string `json:"url"`
 }
 
 func SearchBing(keyword string, nbImages int) ([]ImageObject, error) {
 	query := strings.ReplaceAll(keyword, " ", "+")
 	url := fmt.Sprintf("https://www.bing.com/images/search?q=%s&safeSearch=strict", query)
-	
+
 	resp, err := http.Get(url)
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
-	
+
 	doc, err := goquery.NewDocumentFromReader(resp.Body)
 	if err != nil {
 		return nil, err
 	}
 
 	var imagesObjects []ImageObject
-	
+
 	doc.Find(".iusc").Each(func(i int, s *goquery.Selection) {
 		jsonContent, exists := s.Attr("m")
 		if exists {
 			var result map[string]interface{}
 			err := json.Unmarshal([]byte(jsonContent), &result)
 			if err == nil {
-				if murl, ok := result["murl"].(string); ok {					
+				if murl, ok := result["murl"].(string); ok {
 					imagesObjects = append(imagesObjects, ImageObject{URL: murl})
 				}
 			}
 		}
 	})
-	
+
 	if nbImages > 0 && nbImages < len(imagesObjects) {
 		imagesObjects = imagesObjects[:nbImages]
 	}
