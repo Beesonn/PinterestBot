0c7c59e21f2f58fba3b2e624e034f220669d7d68
Beeson <131391958+Beesonn@users.noreply.github.com>
2025-12-03 12:23:50 +0530

    Updating bing.go file on 2025-12-03

--- settings/bing.go
+++ settings/bing.go
@@ -1,51 +1,51 @@
 package settings
 
 import (
-	"encoding/json"
-	"fmt"
-	"net/http"
-	"strings"
+    "encoding/json"
+    "fmt"
+    "net/http"
+    "strings"
 
-	"github.com/PuerkitoBio/goquery"
+    "github.com/PuerkitoBio/goquery"
 )
 
 type ImageObject struct {
-	URL string `json:"url"`
+    URL string `json:"url"`
 }
 
 func SearchBing(keyword string, nbImages int) ([]ImageObject, error) {
-	query := strings.ReplaceAll(keyword, " ", "+")
-	url := fmt.Sprintf("https://www.bing.com/images/search?q=%s&safeSearch=strict", query)
+    query := strings.ReplaceAll(keyword, " ", "+")
+    url := fmt.Sprintf("https://www.bing.com/images/search?q=%s&safeSearch=strict", query)
 
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
+    resp, err := http.Get(url)
+    if err != nil {
+        return nil, err
+    }
+    defer resp.Body.Close()
 
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
-	if err != nil {
-		return nil, err
-	}
+    doc, err := goquery.NewDocumentFromReader(resp.Body)
+    if err != nil {
+        return nil, err
+    }
 
-	var imagesObjects []ImageObject
+    var imagesObjects []ImageObject
 
-	doc.Find(".iusc").Each(func(i int, s *goquery.Selection) {
-		jsonContent, exists := s.Attr("m")
-		if exists {
-			var result map[string]interface{}
-			err := json.Unmarshal([]byte(jsonContent), &result)
-			if err == nil {
-				if murl, ok := result["murl"].(string); ok {
-					imagesObjects = append(imagesObjects, ImageObject{URL: murl})
-				}
-			}
-		}
-	})
+    doc.Find(".iusc").Each(func(i int, s *goquery.Selection) {
+        jsonContent, exists := s.Attr("m")
+        if exists {
+            var result map[string]interface{}
+            err := json.Unmarshal([]byte(jsonContent), &result)
+            if err == nil {
+                if murl, ok := result["murl"].(string); ok {
+                    imagesObjects = append(imagesObjects, ImageObject{URL: murl})
+                }
+            }
+        }
+    })
 
-	if nbImages > 0 && nbImages < len(imagesObjects) {
-		imagesObjects = imagesObjects[:nbImages]
-	}
+    if nbImages > 0 && nbImages < len(imagesObjects) {
+        imagesObjects = imagesObjects[:nbImages]
+    }
 
-	return imagesObjects, nil
+    return imagesObjects, nil
 }
